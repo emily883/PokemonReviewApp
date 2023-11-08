507101c274838212adbdc452c9a74d2c4c0353ac
Emily <emily.moraima.cruz@hotmail.com>
2023-11-08 13:55:04 -0400

    Adding more get methods

--- Interfaces/IPokemonRepository.cs
+++ Interfaces/IPokemonRepository.cs
@@ -6,5 +6,13 @@ namespace PokemonReviewApp.Interfaces
     {
 
         ICollection<Pokemon> GetPokemons();
+
+        Pokemon GetPokemon(int id);
+                
+        Pokemon GetPokemon(string name);
+
+        decimal GetPokemonRating(int pokeId);
+
+        bool PokemonExists(int pokeId);
     }
 }
--- Repository/PokemonRepository.cs
+++ Repository/PokemonRepository.cs
@@ -13,10 +13,29 @@ namespace PokemonReviewApp.Repository
 
         }
 
+        public Pokemon GetPokemon(int id)
+        {
+            return _context.Pokemons.Where(p => p.Id == id).FirstOrDefault();
+        }
+
+        public Pokemon GetPokemon(string name)
+        {
+            return _context.Pokemons.Where(p => p.Name == name).FirstOrDefault();
+        }
+
+        public decimal GetPokemonRating(int pokeId)
+        {
+            throw new NotImplementedException();
+        }
+
         public ICollection<Pokemon> GetPokemons()
         {
             return _context.Pokemons.OrderBy(p => p.Id).ToList();
         }
 
+        public bool PokemonExists(int pokeId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
